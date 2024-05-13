85061ce49610093a23abe25e9deba9793c710612
Vladimir Kuyanov <shuxov_dmitrij@bk.ru>
2024-05-13 12:57:29 +0300

    Login

--- src/main/java/com/airlines/booking/controllers/LoginController.java
+++ src/main/java/com/airlines/booking/controllers/LoginController.java
@@ -1,10 +1,15 @@
 package com.airlines.booking.controllers;
 
+import com.airlines.booking.models.User;
 import com.airlines.booking.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 public class LoginController {
@@ -15,5 +20,13 @@ public class LoginController {
     public String Login(Model model) {
         model.addAttribute("title", "Login");
         return "Login";
+    }
+
+    @PostMapping("/Login")
+    public String LoginCheck(@RequestParam String email, @RequestParam String password, Model model) {
+        User tUser = userRepository.findByEmail(email);
+        if (tUser == null) return "redirect:/1";
+        if (!Objects.equals(tUser.getPassword(), password)) return "redirect:/2";
+        return "redirect:/";
     }
 }
